6c7718cf572c0554a085fd7f9c39075cb14c4204
Venkat Bhoogarbh <venkatbhoogarbh@gmail.com>
2026-01-25 15:25:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,11 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("password-tool").then(cache => {
       return cache.addAll([
-        "index.html",
+        "./",  // Cache root
         "style.css",
         "script.js"
-      ]);
+      ].catch(() => console.log("Some files failed to cache")))  // Graceful fallback
+        .catch(() => console.log("Cache failed"))
     })
   );
 });
@@ -14,6 +15,6 @@ self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(response => {
       return response || fetch(e.request);
-    })
+    }).catch(() => fetch(e.request))
   );
 });
